1e8173554c4e3a4f6e02d1f8d043f128043f86a1
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-07-09 10:58:10 +0530

    update

--- src/app/component/parent/parent.component.ts
+++ src/app/component/parent/parent.component.ts
@@ -2,14 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/service/user/user.service';
 import { IUser } from 'src/app/interface/user/user';
 import { Laptop } from 'src/app/classes/laptop';
+import { HookLogger, Readonly } from 'src/app/decorators/class.decorator';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss']
 })
+@HookLogger({
+  hookes:[
+    'ngOnInit'
+  ]
+})
+
 export class ParentComponent implements OnInit {
 
+  @Readonly('this is a read only string') readonly:string;
   dateToday: Date;
   users:IUser[];
   filterIdType:string = '';
@@ -19,8 +27,10 @@ export class ParentComponent implements OnInit {
     this.dateToday = new Date();
     this.users = this.userService.getusers();
     let laptop: Laptop = new Laptop;
-    console.log(laptop['stickers']);
-
+    //console.log(laptop['stickers']);
+     console.log(this.readonly);
+     this.readonly = 'This was changed';
+     console.log(this.readonly);
   }
   addUser(){
     this.userService.addUser({
--- src/app/decorators/class.decorator.ts
+++ src/app/decorators/class.decorator.ts
@@ -1,6 +1,44 @@
+
 export function Sticker(configuration) {
- // target.prototype.stickers = ['Github','Google','Kaizer'];
- return function(target) {
-  target.prototype.stickers = configuration.stickers;
- }
+  // target.prototype.stickers = ['Github','Google','Kaizer'];
+  return function (target) {
+    target.prototype.stickers = configuration.stickers;
+  };
+}
+export function HookLogger(configuration?) {
+  return function(target){
+
+    const componentName= target.name;
+    const defaulthooks:string[]= [
+      'ngOnChanges',
+      'ngOnInit',
+      'ngDoCheck',
+      'ngAfterContentInit',
+      'ngAfterContentInChecked',
+      'ngAfterViewInit',
+      'ngAfterViewInChecked',
+      'ngOnDestroy'
+    ];
+
+    let hookstobelogged = (configuration && configuration.hooks) || defaulthooks;
+
+    hookstobelogged.forEach(hooktobelogged => {
+        const original = target.prototype[hooktobelogged];
+        target.prototype[hooktobelogged] = function(...args){
+          console.log(`component name: ${componentName } |  Hooks name: ${hooktobelogged} |`, ...args);
+          original && original.apply(this,args);
+        }
+    });
+
+  }
+}
+
+export function Readonly(value){
+  return function(target,key){
+    Object.defineProperty(target,key,{
+      set: () => target.key = value,
+      get: () => value,
+      configurable:false
+    });
+  }
 }
